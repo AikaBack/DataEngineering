4c6c48c9f0e611148b588153ef4366a6213fb405
AikaBack <aikaback@gmail.com>
2024-01-15 23:18:25 +0500

    push 4 задания 1 и 2

---
+++ 4/1,2/task1.py
@@ -0,0 +1,74 @@
+import sqlite3
+import msgpack
+import json
+
+
+# Функция чтения файла формата msgpack в базу данных
+def read_msgpack_to_sqlite(file_path, db_path):
+    conn = sqlite3.connect(db_path + '.db')
+    cursor = conn.cursor()
+
+    with open(file_path, 'rb') as file:
+        data = msgpack.unpack(file)
+        for row in data:
+            values = (
+                row['name'],
+                row['street'],
+                row['city'],
+                row['zipcode'],
+                row['floors'],
+                row['year'],
+                row['parking'],
+                row['prob_price'],
+                row['views']
+            )
+            cursor.execute(
+                "INSERT INTO building (name, street, city, zipcode, floors, year, parking, prob_price, views) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                values
+            )
+
+    conn.commit()
+    conn.close()
+
+
+# Функция вызова 3 запросов
+def execute_queries(db_path):
+    conn = sqlite3.connect(db_path + '.db')
+    cursor = conn.cursor()
+
+    VAR = 52
+
+    # Вывод первых (VAR+10) отсортированных по полю views строк из таблицы в файл формата json
+    cursor.execute("SELECT * FROM building ORDER BY views LIMIT ?", (VAR + 10,))
+    with open('task1_sorted_views_var_52.json', 'w') as file:
+        json.dump(cursor.fetchall(), file, ensure_ascii=False)
+
+    # Вывод (сумма, минимум, максимум, среднее) по полю prob_price с именами статистики
+    cursor.execute("SELECT SUM(prob_price), MIN(prob_price), MAX(prob_price), AVG(prob_price) FROM building")
+    prob_price_stats = cursor.fetchone()
+
+    print("Статистика цен:")
+    print(f"Cумма: {prob_price_stats[0]}")
+    print(f"Минимальная цена: {prob_price_stats[1]}")
+    print(f"Максимальная цена: {prob_price_stats[2]}")
+    print(f"Средняя цена: {prob_price_stats[3]}")
+
+    # Вывод частоты встречаемости для поля parking
+    cursor.execute("SELECT parking, COUNT(*) FROM building GROUP BY parking")
+    parking_frequency = cursor.fetchall()
+    print("Частота встречаемости парковок:", parking_frequency)
+
+    # Вывод первых (VAR+10) отфильтрованных по условию этажность >=5, сортированных по убывающей цене
+    cursor.execute("SELECT * FROM building WHERE floors >= 5 ORDER BY prob_price DESC LIMIT ?", (VAR + 10,))
+    with open('task1_filtered_floors5_var_52.json', 'w') as file:
+        json.dump(cursor.fetchall(), file, ensure_ascii=False)
+
+    conn.close()
+
+
+# Вызов функций
+file_path_msgpack = 'task_1_var_52_item.msgpack'
+db_path = 'first'
+
+
+execute_queries(db_path)
---
+++ 4/1,2/task2.py
@@ -0,0 +1,60 @@
+import sqlite3
+import msgpack
+import json
+
+
+# Функция для чтения данных из файла msgpack и записи в таблицу building_feedback
+def read_feedback_to_sqlite(file_path, db_path):
+    conn = sqlite3.connect(db_path + '.db')
+    cursor = conn.cursor()
+
+    with open(file_path, 'rb') as file:
+        data = msgpack.unpack(file)
+        for row in data:
+            values = (
+                row['name'],  # Название здания, к которому относится отзыв
+                row['rating'],
+                row['convenience'],
+                row['security'],
+                row['functionality'],
+                row['comment']
+            )
+            cursor.execute(
+                "INSERT INTO building_feedback (building_name, rating, convenience, security, functionality, comment) VALUES (?, ?, ?, ?, ?, ?)",
+                values
+            )
+
+    conn.commit()
+    conn.close()
+
+
+# Функция выполнения запросов с использованием связей между таблицами
+def execute_queries_with_relations(db_path):
+    conn = sqlite3.connect(db_path + '.db')
+    cursor = conn.cursor()
+
+    # Пример запроса: выборка отзывов для определенного здания по его названию
+    cursor.execute("SELECT * FROM building_feedback WHERE building_name = 'Бункер 37'")
+    with open('task2_feedback_specific_building.json', 'w') as file:
+        json.dump(cursor.fetchall(), file, ensure_ascii=False)
+
+    # Пример запроса: подсчет средней оценки (рейтинга) здания на основе отзывов
+    cursor.execute("SELECT AVG(rating) FROM building_feedback WHERE building_name = 'Бункер 37'")
+    with open('task2_average_rating_specific_building.json', 'w') as file:
+        json.dump({'Средняя оценка дома Бункер 37': cursor.fetchone()[0]}, file, ensure_ascii=False)
+
+    # Пример запроса: выборка зданий с комментарием о безопасности выше определенного уровня
+    cursor.execute("SELECT building.* FROM building JOIN building_feedback ON building.name = building_feedback.building_name WHERE building_feedback.security > 2")
+    with open('task2_security2_buildings_comments.json', 'w') as file:
+        json.dump(cursor.fetchall(), file, ensure_ascii=False)
+
+    conn.close()
+
+
+# Вызов функции для чтения данных из msgpack и записи в таблицу
+file_path_msgpack_feedback = 'task_2_var_52_subitem.msgpack'
+db_path = 'first'
+read_feedback_to_sqlite(file_path_msgpack_feedback, db_path)
+
+# Вызов функции для выполнения запросов с использованием связей между таблицами
+execute_queries_with_relations(db_path)
